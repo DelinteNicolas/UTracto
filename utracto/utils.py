e46e9c1fefbd4ec65bcfe307068c66b72180edbe
DelinteNicolas <nicolas.delinte@uclouvain.be>
2022-11-24 17:03:21 +0100

    Reworked conversion from dipy to mrtrix and vice verso

--- utracto/utils.py
+++ utracto/utils.py
@@ -1,3 +1,60 @@
+import numpy as np
+from tqdm import tqdm
+from dipy.data import get_sphere
+from dipy.reconst.shm import sh_to_sf, sf_to_sh
+from dipy.io.streamline import load_tractogram, save_tractogram
+from TIME.core import angle_difference
+
+
+def _flip_m_neg(sh, sh_order, full_basis=False):
+
+    counter = 0
+    for l in range(sh_order.astype(int)):
+        n = 1+2*l
+        m_list = np.linspace((n-1)/2, -(n-1)/2, n)
+        for m in m_list:
+            if full_basis:
+                if m % 2 == 0 and m < 0:
+                    sh[:, :, :, counter] *= -1
+                counter += 1
+            else:
+                if l % 2 == 0:
+                    if m % 2 == 0 and m < 0:
+                        sh[:, :, :, counter] *= -1
+                    counter += 1
+
+    return sh
+
+
+def dipy_fod_to_mrtrix(sh):
+    '''
+    Does not work with full basis, only symmetrical SH
+
+    Parameters
+    ----------
+    sh : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+
+    sh_order = (np.sqrt(sh.shape[3]*8+1)-3)/2
+
+    sh = _flip_m_neg(sh, sh_order)
+
+    default_sphere = get_sphere('repulsion724')
+
+    temp = sh_to_sf(sh, sphere=default_sphere, sh_order=sh_order,
+                    basis_type='descoteaux07', legacy=False)
+    sh = sf_to_sh(temp, sphere=default_sphere, sh_order=sh_order,
+                  basis_type='tournier07', legacy=False)
+
+    return sh
+
+
 def mrtrix_fod_to_dipy(sh):
     '''
     Does not work with full basis, only symmetrical SH
@@ -22,72 +79,64 @@ def mrtrix_fod_to_dipy(sh):
     sh = sf_to_sh(temp, sphere=default_sphere, sh_order=sh_order,
                   basis_type='descoteaux07', legacy=False)
 
-    counter = 0
-    for l in range(sh_order.astype(int)):
-        n = 1+2*l
-        m_list = np.linspace((n-1)/2, -(n-1)/2, n)
-        for m in m_list:
-            if l % 2 == 0 and m % 2 == 0 and m < 0:
-                sh[:, :, :, counter] *= -1
-            if l % 2 == 0:
-                counter += 1
+    sh = _flip_m_neg(sh, sh_order)
 
     return sh
 
-def angle_difference(v1, v2) -> float:
+
+def _find_low_nearest(array, value: int) -> int:
     '''
-    Computes the angle difference between two vectors.
+    Finds the index of the array element closest to value but not above value.
+
     Parameters
     ----------
-    v1 : 1-D array
-        Vector. Ex: [1,1,1]
-    v2 : 1-D array
-        Vector. Ex: [1,1,1]
+    array : 1-D array
+        1-D array
+    value : int
+        Value
+
     Returns
     -------
-    ang : float
-        Angle difference (in degrees).
+    idx : int
+        Index
+
     '''
-
-    v1n = v1/np.linalg.norm(v1)
-    v2n = v2/np.linalg.norm(v2)
-
-    if (v1n == v2n).all():
-        return 0
-
-    if sum(v1n*v2n) > 1:
-        return 0
-
-    if sum(v1n*v2n) < -1:
-        return 180
-
-    ang = np.arccos(sum(v1n*v2n))*180/np.pi
-
-    return ang
-
-
-def find_low_nearest(array, value):
     vals = array-value
     vals[vals > 0] = np.min(vals)
     idx = vals.argmax()
     return idx
 
 
-if __name__ == '__main__':
+def fix_angle(trk_file: str, out_file: str = None, max_angle: int = 30):
+    """
+    Maxes sure no angle between two tractography steps are higher than
+    max_angle. Angles greater than the specified value are cut into two
+    separate streamlines.
 
-    trk_file = "C:/Users/nicol/Documents/Doctorat/Data/disco_phantom/DiSCo1/low_resolution_20x20x20/output/lowRes_DiSCo1_DWI_tract_mrtrix_30.trk"
+    Parameters
+    ----------
+    trk_file : str
+        File path to .trk file.
+    out_file : str, optional
+        If specified, output path. If not specified, overwrites input file.
+        The default is None.
+    max_angle : int, optional
+        Maximum angle between two tractography steps. The default is 30.
+
+    Returns
+    -------
+    None.
+
+    """
 
     trk = load_tractogram(trk_file, 'same')
     trk.to_vox()
     trk.to_corner()
 
-    # -------------------------------------------------
-
     streams = trk.streamlines
     streams_data = trk.streamlines.get_data()
 
     for p in tqdm(range(1, streams_data.shape[0]-1)):
-        # for p in tqdm(range(1, 1000)):
 
         if p in streams._offsets or p+1 in streams._offsets:
             continue
@@ -101,9 +150,9 @@ if __name__ == '__main__':
 
         ang = angle_difference(v1, v2)
 
-        if ang > 30:
+        if ang > max_angle:
 
-            stream_num = find_low_nearest(streams._offsets, p+1)
+            stream_num = _find_low_nearest(streams._offsets, p+1)
 
             streams._offsets = np.insert(streams._offsets, stream_num+1, p+1)
 
@@ -114,5 +163,7 @@ if __name__ == '__main__':
             streams._lengths = np.insert(streams._lengths, stream_num+1,
                                          old_value-new_value)
 
-    save_tractogram(
-        trk, "C:/Users/nicol/Documents/Doctorat/Data/disco_phantom/DiSCo1/low_resolution_20x20x20/output/lowRes_DiSCo1_DWI_tract_mrtrix_30.trk")
+    if out_file is None:
+        out_file = trk_file
+
+    save_tractogram(trk, out_file)
