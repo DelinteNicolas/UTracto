50ecea6a194f7d52e5d16f0a85974306cff15268
DelinteNicolas <nicolas.delinte@uclouvain.be>
2023-05-10 14:57:04 +0200

    Added binama dependency

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
                       'nibabel',
                       'numpy',
                       'tqdm',
+			    'binama',
                       ],
 
     classifiers=['Development Status :: 4 - Beta',
