707bbf6bf6f9d732e65a3871be0fdb95b8e28a0a
DelinteNicolas <nicolas.delinte@uclouvain.be>
2023-05-10 14:13:16 +0200

    v0.0.2

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: DELINTE Nicolas
+"""
+
+from setuptools import setup
+
+import utracto
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "docs/README.md").read_text()
+
+setup(
+    name='utracto',
+    version=utracto.__version__,
+    description='Implementation of UTracto',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/DelinteNicolas/UTracto',
+    author='Nicolas Delinte',
+    author_email='nicolas.delinte@uclouvain.be',
+    license='GNU General Public License v3.0',
+    packages=['utracto'],
+    install_requires=['dipy',
+                      'nibabel',
+                      'numpy',
+                      'tqdm',
+                      ],
+
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Science/Research',
+                 'Natural Language :: English',
+                 'Programming Language :: Python'],
+)
