08545980936b13c1cdd5ec0639f73da0510d149b
Nicolas Delinte <nicolas.delinte@uclouvain.be>
2024-05-08 03:53:33 +0200

    Added grey matter and csf segmentation as inputs for wmfod to angle map

--- utracto/core.py
+++ utracto/core.py
@@ -6,14 +6,15 @@
 
 import numpy as np
 from scipy.ndimage import gaussian_filter
-from binama.utils import erosion
+from binama.utils import dilation
 
 
-def wmfod_to_angle(wmfod, max_value: int = 30, min_value: int = 15,
+def wmfod_to_angle(wmfod, gmfod=None, csffod=None,
+                   max_angle: int = 30, min_angle: int = 15,
                    wm_edge: float = 0.1, interface: float = 0.08,
-                   interface_thickness: int = 3, wm_deep=0.35,
+                   interface_thickness: int = 2, wm_deep=0.35,
                    smooth_factor: float = .85, float_type: bool = False,
-                   only_WM: bool = False):
+                   only_WM: bool = False, more_wm: bool = False):
     """
     Creates an angular map based on the white matter FOD.
 
@@ -21,9 +22,13 @@ def wmfod_to_angle(wmfod, max_value: int = 30, min_value: int = 15,
     ----------
     wmfod : 4-D array of shape (x,y,z,sh)
         Array containg the white matter FOD's spherical harmonics coefficients.
-    max_value : int, optional
+    gmfod : 4-D array of shape (x,y,z,1)
+        Array containg the grey matter FOD's spherical harmonics coefficients.
+    csffod : 4-D array of shape (x,y,z,1)
+        Array containg the csf FOD's spherical harmonics coefficients.
+    max_angle : int, optional
         Maximum value of the tractography angle. The default is 15.
-    min_value : int, optional
+    min_angle : int, optional
         Minimum value of the tractography angle. The default is 30.
     wm_edge : float, optional
         Minimum value of wmfod[:,:,:,0] to be considered as white mater.
@@ -42,6 +47,8 @@ def wmfod_to_angle(wmfod, max_value: int = 30, min_value: int = 15,
     only_WM : bool, optional
         If True, the angular map is only in areas of white matter.
         The default is False.
+    more_wm : bool, optional
+        If true, adds back areas of deep white matter.
 
     Returns
     -------
@@ -54,19 +61,22 @@ def wmfod_to_angle(wmfod, max_value: int = 30, min_value: int = 15,
 
     params = locals()
     del params['wmfod']
-    angle_array = np.where(wmfod[:, :, :, 0] > wm_edge, min_value, max_value)
-    mask = np.where(wmfod[:, :, :, 0] > interface, 1, 0)
-    eroded_mask = erosion(mask.copy(), interface_thickness)
-    tot_mask = mask-eroded_mask
-    angle_array[tot_mask == 1] = max_value
-    sumfod = np.max(abs(wmfod), axis=3)
-    highmask = np.where(sumfod > wm_deep, 1, 0)
-    angle_array[highmask == 1] = min_value
-    if float_type:
-        angle_array = gaussian_filter(angle_array.astype(float), smooth_factor)
+    del params['gmfod']
+    del params['csffod']
+    if gmfod is not None and csffod is not None:
+        seg = np.argmax(np.stack([wmfod[..., 0], gmfod[..., 0], csffod[..., 0]],
+                                 axis=-1), axis=-1)
     else:
-        angle_array = gaussian_filter(angle_array, smooth_factor)
+        seg = np.where(wmfod[..., 0] < interface, 1, 0)
+    gm_mask = np.where(seg == 1, 1, 0)
+    gm_ext = dilation(gm_mask, interface_thickness)
+    angle = np.where(gm_ext == 1, max_angle, min_angle)
+    if more_wm:
+        angle = np.where(np.max(abs(wmfod), axis=3) > wm_deep, min_angle, angle)
+    if float_type:
+        angle = angle.astype(float)
+    amap = gaussian_filter(angle, smooth_factor).astype(float)
     if only_WM:
-        angle_array[wmfod[:, :, :, 0] < wm_edge] = None
+        amap[wmfod[..., 0] < wm_edge] = None
 
-    return angle_array, params
+    return amap, params
